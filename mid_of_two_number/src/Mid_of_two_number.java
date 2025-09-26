a78c65262a6da17235b065130fa6009a9b96f913
MohammadTamjeed <tamjeednasery0@gmail.com>
2025-09-26 21:09:24 +0430

    Add files via upload

---
+++ mid_of_two_number/src/Mid_of_two_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Mid_of_two_number {
+
+    public static void main(String[] args) {
+       Scanner x=new Scanner(System.in);
+        System.out.println("please enter your first number");double num1=x.nextDouble();
+        System.out.println("enter your second number");  double num2=x.nextDouble();
+  double mideem=(num1+num2)/2;
+  
+        System.out.println("the mid of your number is "+mideem);
+    }
+     
+}
